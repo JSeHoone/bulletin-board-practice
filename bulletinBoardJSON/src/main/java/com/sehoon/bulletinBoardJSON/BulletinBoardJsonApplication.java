fb8e312308377c87dd2f70926fe0998e55b7651c
JSeHoone <beebea545@gmail.com>
2025-11-01 22:27:37 +0900

    first init

---
+++ bulletinBoardJSON/src/main/java/com/sehoon/bulletinBoardJSON/BulletinBoardJsonApplication.java
@@ -0,0 +1,13 @@
+package com.sehoon.bulletinBoardJSON;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinBoardJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulletinBoardJsonApplication.class, args);
+	}
+
+}
---
+++ bulletinBoardJSON/src/test/java/com/sehoon/bulletinBoardJSON/BulletinBoardJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sehoon.bulletinBoardJSON;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinBoardJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
